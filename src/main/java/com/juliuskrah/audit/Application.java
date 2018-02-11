ae1103624f7bef13aaf06e3783f4cff8d9827b7b
Julius Krah <juliuskrah@gmail.com>
2018-02-11 17:49:43 +0000

    Created branch 'Initial'

--- src/main/java/com/juliuskrah/audit/Application.java
+++ src/main/java/com/juliuskrah/audit/Application.java
@@ -1,16 +1,7 @@
 package com.juliuskrah.audit;
 
-import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-
-import java.security.Principal;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication
 public class Application {
@@ -18,11 +9,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	@Bean
-	RouterFunction<ServerResponse> helloRouter() {
-		return route(GET("/"), (request) -> ServerResponse.ok()
-				.body(fromPublisher(request.principal()
-						.map(Principal::getName), String.class)));
-	}
+
 }
