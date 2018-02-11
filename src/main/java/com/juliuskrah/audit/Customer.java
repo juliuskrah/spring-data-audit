d7ca07489953f370d1f852d55b583d3cf6a6df8a
Julius Krah <juliuskrah@gmail.com>
2018-02-11 23:26:01 +0000

    Added EntityListeners

--- src/main/java/com/juliuskrah/audit/Customer.java
+++ src/main/java/com/juliuskrah/audit/Customer.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
@@ -13,11 +14,13 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Data;
 
 @Data
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Customer implements Serializable {
 
 	private static final long serialVersionUID = 265315120455998196L;
--- src/test/java/com/juliuskrah/audit/ApplicationTests.java
+++ src/test/java/com/juliuskrah/audit/ApplicationTests.java
@@ -1,16 +1,29 @@
 package com.juliuskrah.audit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
-
+	@Autowired
+	private CustomerRepository repository;
+	
 	@Test
 	public void contextLoads() {
+		Customer customer = new Customer();
+		customer.setName("James Gunn");
+		customer.setEmail("jamesgunn@gmail.com");
+		
+		repository.save(customer);
+		
+		assertThat(customer.getId()).isNotNull();
+		assertThat(customer.getCreatedBy()).isSameAs("system");
 	}
 
 }
