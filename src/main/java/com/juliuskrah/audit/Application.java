e2e43f30d1ed0b192d1ca21d18b8d4c92dcae2ea
Julius Krah <juliuskrah@gmail.com>
2018-02-14 16:58:16 +0000

    Added handler function

--- src/main/java/com/juliuskrah/audit/Application.java
+++ src/main/java/com/juliuskrah/audit/Application.java
@@ -1,8 +1,22 @@
 package com.juliuskrah.audit;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
@@ -11,5 +25,14 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
+	@Bean
+	public RouterFunction<ServerResponse> customerRouter(CustomerHandler customerHandler) {
+		return nest(path("/customers"),
+				route(GET("/").and(accept(APPLICATION_JSON)), customerHandler::retrieveCustomers)
+						.andRoute(GET("/{id}").and(accept(APPLICATION_JSON)), customerHandler::retrieveCustomer)
+						.andRoute(POST("/").and(contentType(APPLICATION_JSON)), customerHandler::createCustomer)
+						.andRoute(PUT("/{id}").and(contentType(APPLICATION_JSON)), customerHandler::updateCustomer)
+						.andRoute(DELETE("/{id}"), customerHandler::deleteCustomer));
+	}
 }
--- src/main/java/com/juliuskrah/audit/SpringSecurityAuditorAware.java
+++ src/main/java/com/juliuskrah/audit/SpringSecurityAuditorAware.java
@@ -1,25 +1,25 @@
 package com.juliuskrah.audit;
 
-import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Component;
+
+import reactor.core.publisher.Mono;
 
 @Component
 public class SpringSecurityAuditorAware implements AuditorAware<String> {
-
+		
 	@Override
 	public Optional<String> getCurrentAuditor() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-	    if (authentication == null || !authentication.isAuthenticated()) {
-	      return Optional.of("system");
-	    }
-
-	    return  Optional.ofNullable(
-	    		((Principal)authentication.getPrincipal()).getName());
+		return ReactiveSecurityContextHolder.getContext()
+				.map(SecurityContext::getAuthentication)
+				.filter(Authentication::isAuthenticated)
+				.map(Authentication::getName)
+				.switchIfEmpty(Mono.just("julius"))
+				.blockOptional();
 	  }
 }
