53b6cb7a68c365d3f2e3bfa0492c30214960cbe3
Julius Krah <juliuskrah@gmail.com>
2018-03-06 14:59:55 +0000

    Initial for Hibernate Envers

--- src/test/java/com/juliuskrah/audit/ApplicationTests.java
+++ src/test/java/com/juliuskrah/audit/ApplicationTests.java
@@ -1,16 +1,15 @@
 package com.juliuskrah.audit;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@SpringJUnitConfig
 public class ApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testAudits() {
 	}
 
 }
